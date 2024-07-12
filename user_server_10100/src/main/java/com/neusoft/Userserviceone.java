a1caf42fce95e9fb025f47533d7f29bb0840b02a
Lsysxn <130546119+Lsysxn@users.noreply.github.com>
2024-07-12 21:19:01 +0800

    Add files via upload

---
+++ user_server_10100/src/main/java/com/neusoft/Userserviceone.java
@@ -0,0 +1,11 @@
+package com.neusoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Userserviceone {
+    public static void main(String[] args) {
+        SpringApplication.run(Userserviceone.class,args);
+    }
+}
---
+++ user_server_10100/src/main/java/com/neusoft/service/UserService.java
@@ -0,0 +1,7 @@
+package com.neusoft.service;
+import com.neusoft.po.User;
+public interface UserService {
+    public User getUserByIdByPass(User user);
+    public int getUserById(String userId);
+    public int saveUser(User user);
+}
